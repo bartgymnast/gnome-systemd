266086d98466fe3b650471fe24b365f98569bb76
Eugene Wissner <belka.ew@gmail.com>
2014-05-12 16:17:52 +0200

    Write cracklib build script

--- cracklib/doinst.sh
+++
@@ -1,3 +0,0 @@
-if [ -x bin/chroot ]; then
-	bin/chroot /$ROOT /usr/sbin/cracklib-format /usr/share/cracklib/* | /usr/sbin/cracklib-packer 2>/dev/null 1>/dev/null
-fi
---
+++ system/cracklib/doinst.sh
@@ -0,0 +1,1 @@
+usr/sbin/cracklib-format usr/share/cracklib/* | usr/sbin/cracklib-packer >/dev/null 2>&1
