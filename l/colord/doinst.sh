9cee053e326f8f411c8d6266a8a1e9d058184619
Bart van der Hall <bartgymnast@hotmail.com>
2014-10-26 01:08:58 +0200

    Updated colord-gtk and colord

--- l/colord/doinst.sh
+++ l/colord/doinst.sh
@@ -1,6 +1,6 @@
 function free_user_id {
   # Find a free user-ID >= 100 (should be < 1000 so it's not a normal user)
-  local FREE_USER_ID=120
+  local FREE_USER_ID=100
   while grep --quiet "^.*:.*:${FREE_USER_ID}:.*:.*:.*:" etc/passwd; do
     let FREE_USER_ID++
   done
@@ -8,7 +8,7 @@ function free_user_id {
 }
 function free_group_id {
   # Find a free group-ID >= 120 (should be < 1000 so it's not a normal group)
-  local FREE_GROUP_ID=120
+  local FREE_GROUP_ID=100
   while grep --quiet "^.*:.*:${FREE_GROUP_ID}:" etc/group; do
     let FREE_GROUP_ID++
   done
@@ -40,9 +40,9 @@ then
   USER_ID=$(echo ${OLD_ENTRY} | cut --fields=3 --delimiter=':')
   test ${USER_ID} -ge 1000 && USER_ID=$(free_user_id)
   if test "${OLD_USER}" = "colord"; then
-    echo -n "Updating unprivileged user" 1>&2
+    echo -n "Updating unprivileged user " 1>&2
   else
-    echo -ne "Changing unprivileged user \e[1m${OLD_USER}\e[0m to" 1>&2
+    echo -ne "Changing unprivileged user \e[1m${OLD_USER}\e[0m to " 1>&2
   fi
   ${CHROOT} /usr/sbin/usermod \
       -d '/var/lib/colord' \
@@ -52,7 +52,7 @@ then
       ${OLD_USER}
 else
   # Add new user
-  echo -n "Creating unprivileged user" 1>&2
+  echo -n "Creating unprivileged user " 1>&2
   ${CHROOT} /usr/sbin/useradd \
     -c 'User for colord' \
     -u $(free_user_id) \
@@ -63,17 +63,22 @@ else
 fi
 
 config() {
-  NEW="\$1"
-  OLD="\$(dirname \$NEW)/\$(basename \$NEW .new)"
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
-  if [ ! -r \$OLD ]; then
-    mv \$NEW \$OLD
-  elif [ "\$(cat \$OLD | md5sum)" = "\$(cat \$NEW | md5sum)" ]; then
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
-    rm \$NEW
+    rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
+## List of conf files to check.  The conf files in your package should end in .new
+config var/lib/colord/mapping.db.new
+config var/lib/colord/storage.db.new
+rm -f var/lib/colord/mapping.db.new
+rm -f var/lib/colord/storage.db.new
 
 # Fix permissions
 ${CHROOT} /bin/chown colord.colord /var/lib/colord
