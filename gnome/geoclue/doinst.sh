5207d9119494a2ec49b61e37a030ffd1d72bd0e8
Bart van der Hall <bartgymnast@hotmail.com>
2014-10-26 13:21:13 +0100

    geoclue doinst.sh fixes

--- gnome/geoclue/doinst.sh
+++ gnome/geoclue/doinst.sh
@@ -62,14 +62,14 @@ if [ -x bin/systemctl ] ; then
 fi
 
 config() {
-  NEW="\$1"
-  OLD="\$(dirname \$NEW)/\$(basename \$NEW .new)"
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
   # If there's no config file by that name, mv it over:
-  if [ ! -r \$OLD ]; then
-    mv \$NEW \$OLD
-  elif [ "\$(cat \$OLD | md5sum)" = "\$(cat \$NEW | md5sum)" ]; then
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
     # toss the redundant copy
-    rm \$NEW
+    rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
