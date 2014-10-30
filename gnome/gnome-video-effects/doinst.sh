556ef4b1381ed5d0ca9a0e65418c5cf3ee173295
Bart van der Hall <bartgymnast@hotmail.com>
2014-10-30 22:27:02 +0100

    it was the update-mime-database

--- gnome/gnome-video-effects/doinst.sh
+++ gnome/gnome-video-effects/doinst.sh
@@ -15,6 +15,10 @@ if [ -x /usr/bin/glib-compile-schemas ] ; then
   chroot . /usr/bin/glib-compile-schemas --allow-any-name /usr/share/glib-2.0/schemas &> /dev/null
 fi
 
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
 if [ -x /bin/systemctl ] ; then
   chroot . /bin/systemctl --system daemon-reload >/dev/null 2>&1
 fi
