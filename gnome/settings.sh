3dec4679a47ba0b942139b902985656c5de4c9ff
Bart van der Hall <bartgymnast@hotmail.com>
2015-02-02 17:54:09 +0100

    fix gnome-initial-setup

---
+++ gnome/settings.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# These settings are recommended for a nice working desktop
+#
+# Lets first enable some services
+
+systemctl set-default graphical.target
+
+systemctl enable gdm
+systemctl enable accounts-daemon
+systemctl enable update-gdk-pixbuf-loaders
+systemctl enable update-gio-modules
+systemctl enable update-gtk-immodules-2
+systemctl enable update-gtk-immodules-3
+systemctl enable update-mime-database
+systemctl enable update-pango-querymodules
+systemctl enable upower
+systemctl disable systemd-networkd
+systemctl disable systemd-resolved
+systemctl enable NetworkManager
+systemctl enable canberra-system-bootup
+systemctl enable canberra-system-shutdown-reboot
+systemctl enable canberra-system-shutdown
+systemctl enable glib-compile-schemas
